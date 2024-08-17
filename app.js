b4b1e3e0eab711b925c4c27077c4d20ca3189df0
Kung <kunglee2003@gmail.com>
2024-08-17 13:55:57 -0400

    Setting up front end connections

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// Send the index.html file when accessing the root URL
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
