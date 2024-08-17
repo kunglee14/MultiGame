55a94787da6687b189ebc9ac603040b3969fbd37
Kung <kunglee2003@gmail.com>
2024-08-17 14:24:25 -0400

    Created socket connections

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
+
+console.log("Server Loaded")
 
--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+// const socket = io()
+
+const devicePixelRatio = window.devicePixelRatio || 1
+
+canvas.width = 1024 * devicePixelRatio
+canvas.height = 576 * devicePixelRatio
+
+c.scale(devicePixelRatio, devicePixelRatio)
