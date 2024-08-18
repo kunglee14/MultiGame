33c1983eb16e49dde87a581459014b147965b6aa
Kung <kunglee2003@gmail.com>
2024-08-18 11:56:44 -0400

    Server client interation for moving

--- app.js
+++ app.js
@@ -7,19 +7,56 @@ const port = 3000;
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
-const io = new Server(server)
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+const backendPlayers = {}
+
 io.on('connection', (socket) => {
-    console.log('a user connected')
+    console.log(`a user connected: ${socket.id}`)
+    socket.on('disconnect', (reason) => {
+        console.log(`(${socket.id}) disconnected: ${reason}`)
+        delete backendPlayers[socket.id]
+        // io.emit('updatePlayers', backEndPlayers) //A signal sent to Client
+    })
     socket.on('keydown', ({keycode}) => {
-        console.log(keycode)
+        const SPEED = 10
+        switch(keycode){
+            case 'KeyW':
+                backendPlayers[socket.id].y -= SPEED
+                break
+            case 'KeyA':
+                backendPlayers[socket.id].x -= SPEED
+                break
+
+            case 'KeyS':
+                backendPlayers[socket.id].y += SPEED
+                break
+
+            case 'KeyD':
+                backendPlayers[socket.id].x += SPEED
+                break
+        }
+    })
+
+    socket.on("initGame", ({username, width, height}) =>{
+        backendPlayers[socket.id] = {
+            x: width * Math.random(),
+            y: height * Math.random(),
+            color: `hsl(${360 * Math.random()}, 100%, 30%)`,
+            username: username
+        }
+        io.emit("updatePlayers", backendPlayers)
     })
 })
+
+setInterval(()=>{
+    io.emit("updatePlayers", backendPlayers)
+},20)
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
--- public/index.js
+++ public/index.js
@@ -2,21 +2,20 @@ const canvas = document.querySelector('canvas')
 const body = document.body
 const cxt = canvas.getContext('2d')
 
-// const socket = io()
+const socket = io()
 
 const devicePixelRatio = window.devicePixelRatio
-// console.log(devicePixelRatio)
-// canvas.width = 1024 * devicePixelRatio
-// canvas.height = 768 * devicePixelRatio
+console.log(devicePixelRatio)
+canvas.width = 1024 * devicePixelRatio
+canvas.height = 768 * devicePixelRatio
 // console.log(window.getComputedStyle(body).width, window.getComputedStyle(body).height)
-w = parseFloat(window.getComputedStyle(body).width)
-h = parseFloat(window.getComputedStyle(body).width)
-canvas.width = w
-canvas.height = h
-
+// w = parseFloat(window.getComputedStyle(body).width)
+// h = parseFloat(window.getComputedStyle(body).width)
+// canvas.width = w
+// canvas.height = h
 cxt.scale(devicePixelRatio, devicePixelRatio)
-p1 = new Player(100,200,150,200,"#eb4034","Keeby")
-p1.draw()
+
+const frontendPlayers = {}
 
 const keys = {
     w_pressed : false,
@@ -26,25 +25,22 @@ const keys = {
 }
 
 setInterval(() => {
-    const SPEED = 4
-    p1.remove()
     if (keys.w_pressed) {
-      p1.y += -SPEED
+        socket.emit("keydown",{keycode: "KeyW"})
     }
   
     if (keys.a_pressed) {
-      p1.x += -SPEED
+        socket.emit("keydown",{keycode: "KeyA"})
     }
   
     if (keys.s_pressed) {
-        p1.y += SPEED
+        socket.emit("keydown",{keycode: "KeyS"})
     }
   
     if (keys.d_pressed) {
-        p1.x += SPEED
+        socket.emit("keydown",{keycode: "KeyD"})
     }
-    p1.draw()
-  }, 15)
+  }, 20)
 
 window.addEventListener('keydown', (event) => {    
     switch (event.code) {
@@ -86,42 +82,72 @@ window.addEventListener('keydown', (event) => {
     }
   })
 
+
+
 function updateDirection(event) {
-    p1.mouse_x = event.x
-    p1.mouse_y = event.y
-    p1.remove()
-    p1.draw()
+    const frontendPlayer = frontendPlayers[socket.id]
+    if(!frontendPlayer) return
+    frontendPlayer.mouse_x = event.x
+    frontendPlayer.mouse_y = event.y
 }
              
-function render(bullet) {         
-    const SPEED = 15
-    setTimeout(function() {
-        if (bullet.x < canvas.width && bullet.x > 0 && bullet.y < canvas.height && bullet.y > 0) {           
-            bullet.draw()
-            if (bullet.old_x != null && bullet.old_y != null){
-                bullet.remove()
-            }
-            bullet.old_x = bullet.x
-            bullet.old_y = bullet.y
-            bullet.x += SPEED*Math.cos(bullet.angle)
-            bullet.y -= SPEED*Math.sin(bullet.angle)
-            render(bullet)     
-        }else{
-            bullet.remove()
-        }              
-    }, 20)
-}
+// function render(bullet) {         
+//     const SPEED = 15
+//     setTimeout(function() {
+//         if (bullet.x < canvas.width && bullet.x > 0 && bullet.y < canvas.height && bullet.y > 0) {           
+//             bullet.draw()
+//             if (bullet.old_x != null && bullet.old_y != null){
+//                 bullet.remove()
+//             }
+//             bullet.old_x = bullet.x
+//             bullet.old_y = bullet.y
+//             bullet.x += SPEED*Math.cos(bullet.angle)
+//             bullet.y -= SPEED*Math.sin(bullet.angle)
+//             render(bullet)     
+//         }else{
+//             bullet.remove()
+//         }              
+//     }, 20)
+// }
 var last_shot = 0
 function fire(event) {
     if(Date.now() - last_shot > 500){
         last_shot = Date.now()
-        bullet = p1.shoot(event.x, event.y)
-        render(bullet)
+        // bullet = p1.shoot(event.x, event.y)
+        // render(bullet)
     }
 }
 
 canvas.addEventListener("mousemove", updateDirection, false)
-canvas.addEventListener("mousedown", fire, false)
+// canvas.addEventListener("mousedown", fire, false)
+function getCoords(x,  y, angle, center_to_corner_length) {
+    const x_cord = x + (center_to_corner_length * Math.cos(angle))
+    const y_cord = y - (center_to_corner_length * Math.sin(angle))
+    return [x_cord, y_cord]
+}
+socket.emit("initGame", {username:"Keeby", width:canvas.width, height:canvas.height})
+socket.on("updatePlayers", (backendPlayers) =>{
+    for (const socketId in backendPlayers){
+        const backendPlayer = backendPlayers[socketId]
+        if(!frontendPlayers[socketId]){
+            frontendPlayers[socketId] = new Player(
+                backendPlayer.x,
+                backendPlayer.y,
+                backendPlayer.x+50,
+                backendPlayer.y,
+                backendPlayer.color,
+                backendPlayer.username
+            )
+        }else{
+            frontendPlayers[socketId].remove()
+            frontendPlayers[socketId].x = backendPlayer.x
+            frontendPlayers[socketId].y = backendPlayer.y
+        }
+        // const frontendPlayer = frontendPlayers[socketId]
+        
+        frontendPlayers[socketId].draw()
+    }
+})
 // document.querySelector('#usernameForm').addEventListener('submit', (event) => {
 //     event.preventDefault()
 //     document.querySelector('#usernameForm').style.display = 'none'
